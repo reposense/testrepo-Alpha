4b28cb6fbc6023d44b898f312cee2546aa1dbf99
SkyBlaise <zsc990124@gmail.com>
2023-11-04 15:04:20 +0800

    add 3rd test file for testing annotated author override

---
+++ analyzeAuthorshipTest3.java
@@ -0,0 +1,9 @@
+// @author myteo
+This line is commited by SkyBlaise but attributed to myteo.
+myteo should get partial credit.
+// @@author
+
+// @author SkyBlaise
+This line is commited by SkyBlaise and attributed to SkyBlaise.
+SkyBlaise should get full credit.
+// @@author
