bc763d2ea7a59f66c5a09fb25ace0a554abb9fa5
SkyBlaise <zsc990124@gmail.com>
2023-11-07 19:29:34 +0800

    fix error in author tag

--- analyzeAuthorshipTest3.java
+++ analyzeAuthorshipTest3.java
@@ -1,9 +1,9 @@
-// @author myteo
+// @@author myteo
 This line is commited by SkyBlaise but attributed to myteo.
 myteo should get partial credit.
 // @@author
 
-// @author SkyBlaise
+// @@author SkyBlaise
 This line is commited by SkyBlaise and attributed to SkyBlaise.
 SkyBlaise should get full credit.
 // @@author
