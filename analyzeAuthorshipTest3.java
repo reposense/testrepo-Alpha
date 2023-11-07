372f84db5fe022f6c5c282010e265dc412897ee5
SkyBlaise <zsc990124@gmail.com>
2023-11-07 19:33:52 +0800

    remove space

--- analyzeAuthorshipTest3.java
+++ analyzeAuthorshipTest3.java
@@ -2,7 +2,6 @@
 This line is commited by SkyBlaise but attributed to myteo.
 myteo should get partial credit.
 // @@author
-
 // @@author SkyBlaise
 This line is commited by SkyBlaise and attributed to SkyBlaise.
 SkyBlaise should get full credit.
